be743b5f8510fdb9c0dcf5930802a79f49af2876
Deenow-verse <232454860+Deenow-verse@users.noreply.github.com>
2026-07-11 15:09:07 +0300

    WIP: Initial implementation of insertString - logic has bugs with buffer overflow and over write

---
+++ Char_strings/exercise7-9.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main (void)
+{
+   char text [] = "The wrong son";
+   char add [] = "per";
+   void insertString(char source [], char insert [], int position);
+
+   insertString (text, add , 10);
+
+   printf ("%s\n", text);
+
+   return 0;
+
+} 
+
+int stringlen (char string [])
+{
+    int len = 0;
+    while (string [len] != '\0')
+    {
+        ++len;
+    }
+
+    return len;
+}
+
+void insertString (char source [], char insert [], int position)
+{
+    //shift the char present from source [position] by lenght of insert, start from null the last index and coming back one step till you hit position
+    int insertLen = stringlen (insert);
+    int sourceLen = stringlen (source);
+
+   while (sourceLen >= position)
+   {
+        source [sourceLen] = source [(sourceLen + insertLen)];
+        --sourceLen; 
+   }
+
+   //after moving you now write the values of insert in that space left from position to position + insertLen - 1
+   
+   for (int i = position, j = 0; j < insertLen; ++i, ++j)
+   {
+    source [i] = insert [j];
+   }
+
+
+}
