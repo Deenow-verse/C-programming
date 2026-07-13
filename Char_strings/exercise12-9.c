203c4dd9e1f25d538cda6e55bf74e5598f1ef494
Deenow-verse <232454860+Deenow-verse@users.noreply.github.com>
2026-07-13 08:57:07 +0300

    strToFloat: add support for floating-point decimals
    -Refactors strToFloat to correctly parse numbers with decimal points.
    -Tracks the decimal status using a flag and divides subsequent digits by
    increasing powers of 10 to build the fractional component. Also changes
    the return value calculations to use float types to prevent truncation.

--- Char_strings/exercise12-9.c
+++ Char_strings/exercise12-9.c
@@ -12,26 +12,35 @@ int main(void)
 
 float strToFloat (char string [])
 {
-    int i = 0, intValue, result = 0;
-    _Bool isnegative = 0;
+    int i = 0, intValue;
+    float result = 0.0, divisor = 10.0;
+    _Bool isNegative = 0, hasDecimal;
 
     if (string [0] == '-')
     {
-        isnegative = 1;
+        isNegative = 1;
         i = 1;
     }   
 
-    while ((string [i] >= '0' && string [i] <= '9') || string [i] == '.')
-    {        
-        intValue = string [i] - '0';
-        result = result * 10 + intValue;
+    while (string [i] != '\0')
+    {  
+        if (string [i] == '.')
+        hasDecimal = 1;
+
+        else if (string [i] >= '0' && string [i] <= '9')
+        {
+            if (!hasDecimal)
+            {
+                result = result * 10.0 + (string [i] - '0');
+            }
+            else
+            {
+                result += (string [i] - '0') / divisor;
+                divisor *= 10.0;
+            }
+        }
         ++i;
     }
 
-    if (isnegative)
-    {
-        result = -result;
-    }
-
-    return result;
+    return isNegative ? -result : result;
 }
