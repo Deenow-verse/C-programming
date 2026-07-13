36f0e93d32bc1d45dcfd43a59c3f3968dabc8082
Deenow-verse <232454860+Deenow-verse@users.noreply.github.com>
2026-07-13 11:36:59 +0300

    feat: implement program 10.2 (more pointer basics)
    -Illustrate how indirection operators access and modify th underlaying variable via pointer reference

---
+++ Pointers/more-pointer-basics.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c = 'Q';
+    char *char_ptr;
+    
+    char_ptr = &c;
+
+    printf ("%c  %c\n", c, *char_ptr);
+
+    c = '/';
+    printf ("%c  %c\n", c, *char_ptr);
+
+    *char_ptr = ')';
+    printf ("%c  %c\n", c, *char_ptr);
+
+    return 0;
+}
