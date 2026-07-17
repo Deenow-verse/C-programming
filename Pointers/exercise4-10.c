88de2aec763b3a0e294c769853d69bb6597c100f
Deenow-verse <232454860+Deenow-verse@users.noreply.github.com>
2026-07-17 21:29:29 +0300

    feat: implement removeEntry function for linked lists deletion
    -Create 'removeEntry()' to bypass and remove the node immediately following the passed pointer.
    -Utilize the dummy head node ('listHead') to successfully delete the first actual element in the list ('n1')
    -Verify successful deletion via list traversal and starndard output

---
+++ Pointers/exercise4-10.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+struct entry  
+{
+    int values;
+    struct entry *next;
+}n1, n2, n3, dummyPointer;
+
+int main (void)
+{
+    void removeEntry (struct entry *removeAfter);
+
+    dummyPointer.next = &n1;
+    n1.values = 100;
+    n1.next = &n2;
+
+    n2.values = 200;
+    n2.next = &n3;
+
+    n3.values = 300;
+    n3.next = (struct entry *)0;
+
+    removeEntry (&n1);
+    removeEntry (&dummyPointer);
+
+    while (dummyPointer.next != (struct entry *)0)
+    {
+        printf ("The value of the current entry is %d \n", dummyPointer.next -> values);
+        dummyPointer.next = dummyPointer.next -> next;
+    }
+}
+
+void removeEntry (struct entry *removeAfter)
+{
+    removeAfter -> next = (removeAfter -> next) -> next;
+}
