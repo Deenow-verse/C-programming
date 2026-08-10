cc9ab74c509c8c9951c76795459935dc1a1068eb
Deenow-verse <232454860+Deenow-verse@users.noreply.github.com>
2026-08-11 01:28:55 +0300

    feat(math): define foundational coordinate and color structures
    - Implement Vector2D for spatial positioning.
    - Implement RGBA_Colour for 32-bit color channel management.
    - Implement Rectangle bounds for collision and rendering boundaries.

---
+++ UNDERWORLD_TRACKER/my_math.h
@@ -0,0 +1,21 @@
+typedef struct
+{
+    float x;
+    float y;
+} Vector2D;
+
+typedef struct
+{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned char a;
+} RGBA_Colour;
+
+typedef struct
+{
+    float x;
+    float y;
+    float width;
+    float length;
+} Rectangle;
