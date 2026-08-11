6265d4fe6b9c7aac91666328a901c456254cd87a
Deenow-verse <232454860+Deenow-verse@users.noreply.github.com>
2026-08-12 00:14:12 +0300

    feat(gui): architect initial Underworld Tracker visual layout
    - Establish foundational Cartesian coordinates for main UI panels (sidebar, center stage, grid container).
    - Implement nested matrix loop to render spatial heatmap geometry.
    - Define 32-bit RGBA color palette for UI components.

--- UNDERWORLD_TRACKER/gui.c
+++ UNDERWORLD_TRACKER/gui.c
@@ -6,8 +6,12 @@ int main (void)
     int target_time_per_frame = 16666;
     Engine_InitWindow (800, 600, "Underworld Tracker");
 
-    RGBA_Colour bg = {20, 20, 20, 255};
-    RGBA_Colour paint = {153, 232, 248, 232};
+    RGBA_Colour bg = {255, 255, 255, 255};
+    RGBA_Colour center = {198, 228, 255, 255};
+    RGBA_Colour clock = {246, 114, 56, 255  };
+    RGBA_Colour grid = {236, 236, 242, 255};
+    RGBA_Colour square ={56, 139, 253, 255};
+    RGBA_Colour sidebar = {219, 109, 40, 255};
 
     while (!Engine_WindowShouldClose())
     {
@@ -15,7 +19,21 @@ int main (void)
 
         Engine_ClearBackground (bg);
 
-        Engine_DrawRectangle   (32, 48, 720, 480, paint);
+        Engine_DrawRectangle   (0, 0, 210, 1000, sidebar);
+
+        Engine_DrawRectangle   (700, 720, 900, 240, center);
+
+        Engine_DrawRectangle   (725, 745 , 90, 90, clock);
+
+        Engine_DrawRectangle   (896, 854, 692, 94, grid);
+
+        for (int row = 861; row  < 945; row += 13)
+        {
+            for (int column = 903; column < 1574; column += 13)
+            {
+                Engine_DrawRectangle (column, row, 5, 5, square);
+            }
+        }
 
         Engine_EndDrawing   ();
 
